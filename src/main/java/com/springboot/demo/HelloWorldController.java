a2ad1388f082e4da71f731e102ed273287d26080
周涛 <zhotuaoo@foxmail.com>
2017-05-20 14:53:09 +0800

    升级springboot到1.5版本

--- src/main/java/com/springboot/demo/HelloWorldController.java
+++ src/main/java/com/springboot/demo/HelloWorldController.java
@@ -1,14 +1,13 @@
 package com.springboot.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@Controller
+@RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
@@ -17,7 +16,6 @@ public class HelloWorldController {
 
     @RequestMapping(method = RequestMethod.GET)
     public
-    @ResponseBody
     Greeting sayHello(@RequestParam(value = "name", required = false, defaultValue = "Stranger") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
